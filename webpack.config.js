b9f68d6b60ceb2594d549f22442ddb4b28911813
Juan-Lengyel <juan.lengyel@globant.com>
2020-06-22 13:43:11 -0500

    Corrected webpack

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,15 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/'),
     publicPath: '/dist/',
-    fileName: 'bundle.js'
+    filename: 'bundle.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    contentBase: path.join(__dirname, 'public/'),
     port: 3000,
     publicPath: 'https://localhost:3000/dist/',
     hotOnly: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()]
-}
+};
