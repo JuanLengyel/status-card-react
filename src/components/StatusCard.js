56dd07c006fb36056d9dcb2bfdfb332bdf87008d
Juan-Lengyel <juan.lengyel@globant.com>
2020-06-22 14:59:20 -0500

    Better look and feel of status card

--- src/components/StatusCard.js
+++ src/components/StatusCard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import Comment from './Comment'
+import Comment from './Comment';
+import './StatusCard.css';
 
 class StatusCard extends Component {
   render() {
     return (
-      <div>
+      <div class='statusCard'>
+        <div></div>
         <Comment />
       </div>
     );
