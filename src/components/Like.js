929a4bcf4972069b41b3c197ee4358f586690594
Juan-Lengyel <juan.lengyel@globant.com>
2020-07-01 22:06:01 -0500

    Added interactivity to like button

--- src/components/Like.js
+++ src/components/Like.js
@@ -4,21 +4,35 @@ import icon from './like-reaction-unset.svg';
 import './Like.css';
 
 class Like extends Component {
+  constructor() {
+    super();
+    this.state = {
+      liked: false
+    };
+    this.handleClickLike = this.handleClickLike.bind(this);
+  }
+
   render() {
     return (
       <div>
-        <LikeIcon />
+        {this.state.liked && <LikeIcon />}
 
         <hr></hr>
         
         <div>
-          <button className='likeButton'>
+          <button className='likeButton' onClick={this.handleClickLike}>
             <img className='buttonContent' src={icon}/>
             <span className='buttonContent'>Like</span>
           </button>
         </div>
       </div>
     );
+  }
+
+  handleClickLike(event) {
+    this.setState(previousState => ({
+      liked: !previousState.liked
+    }));
   }
 }
 
