67755eda68e3dbf19308b957aedb6f1e72cddaaa
Juan-Lengyel <juan.lengyel@globant.com>
2020-07-01 20:47:41 -0500

    Adjusted final component

--- src/components/StatusCard.js
+++ src/components/StatusCard.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import Like from './Like';
 import Comment from './Comment';
 import './StatusCard.css';
 
 class StatusCard extends Component {
   render() {
     return (
-      <div class='statusCard'>
-        <div></div>
+      <div className='statusCard'>
+        <div className='mainContainer'>
+          <div className='statusHead'>
+            <div className='imageContainer'></div>
+            <div className='title'>
+              <a href='#'>Master of the Universe</a>
+            </div>
+            <div className='statusTime'>Posted 10 min ago</div>
+          </div>
+          <p>This is my status</p>
+        <Like />
+        </div>
         <Comment />
       </div>
     );
